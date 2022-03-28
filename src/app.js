6e2c295c118cab1d7d809517128778378e2f33b1
Talha Bilal <talhabilal1997@gmail.com>
2022-03-28 22:25:15 +0500

    Modified files for deployment on Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/weatherstack');
 const app = express()
 
+const port = process.env.PORT || 3000
+
 // define paths for express
 const publicDirPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -89,8 +91,8 @@ app.get('*',(req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on 3000')
+app.listen(port, () => {
+    console.log('Server is up on '+port)
 })
 
 
